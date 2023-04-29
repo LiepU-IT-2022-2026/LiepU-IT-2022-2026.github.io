3c4ac28d82c640b060ca164c5d1121b396b26d4d
mAa4a <zobbroslv@gmail.com>
2023-04-30 01:00:43 +0300

    Pačakarējos - still, nav progress

--- Uzdevumi/Latv_val/Pirmais_burts/javascript.js
+++ Uzdevumi/Latv_val/Pirmais_burts/javascript.js
@@ -1,52 +1,107 @@
-$( start );
-
-function start1(){
-$('#Ending1').hide();
-
 // Define an array of picture objects, each containing a URL and a letter.
 const pictures = [
-  {url: "bildes2/abols.jpg", letter: "Ā"},
+  {url: "bildes2/abols.png", letter: "Ā"},
   {url: "picture2.jpg", letter: "B"},
   {url: "picture3.jpg", letter: "C"}
 ];
+var correct = 0;
 
-// Shuffle the array of pictures using the Fisher-Yates algorithm.
-for (let i = pictures.length - 1; i > 0; i--) {
-  const j = Math.floor(Math.random() * (i + 1));
-  [pictures[i], pictures[j]] = [pictures[j], pictures[i]];
-}
+$( start );
 
-// Atsevisks div katrai bildei un input vieta
-const pictureContainer = document.getElementById("picture-container");
-pictures.forEach(pictures => {
-  const div = document.createElement("div");
-  const label = document.createElement("label");
-  label.textContent = `Uz kuru burtu šis auglis sākas?`;
-  const input = document.createElement("input");
-  input.type = "teksts";
-  input.maxLength = 1;
-  input.required = true;
-  const img = document.createElement("img");
-  img.src = pictures.url;
-  img.alt = `Bilde ar kaut ko kas sākas uz burtu: ${pictures.letter}`;
-  div.appendChild(label);
-  div.appendChild(input);
-  div.appendChild(img);
-  pictureContainer.appendChild(div);
-});
-
-function openNav() {
-  document.getElementById("myNav").style.height = "100%";
-}
-function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
-}
-  drop: check
-}
-
+/*
 function check(event, ui) {
-    correct++;
-  if (correct==5){
+  var UserInput = document.getElementById( `${pictures.letter}_input` );
+  var ImageInfo = document.getElementById( `${pictures.letter}` );
+  if (UserInput == ImageInfo){
+    correct += 1;
+  }
+  if (correct == 3){
     $('#Ending').show();
   }
+};*/
+
+function start(){
+  $('#Ending').hide();
+
+  // Pārkārto masīvu
+  for (let i = pictures.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [pictures[i], pictures[j]] = [pictures[j], pictures[i]];
+  }
+
+  // Atsevisks div katrai bildei un input vieta
+  const pictureContainer = document.getElementById("picture-container");
+  //pictureContainer.innerHTML = '';
+  pictures.forEach(pictures => {
+    const div = document.createElement("div");
+    const label = document.createElement("label");
+    label.textContent = `Uz kuru burtu šis auglis sākas?`;
+    const input = document.createElement("input");
+    input.id = `${pictures.letter}_input`;
+    input.type = "teksts";
+    input.maxLength = 1;
+    input.required = true;
+    const img = document.createElement("img");
+    img.src = pictures.url;
+    img.alt = `Bilde ar kaut ko kas sākas uz burtu: ${pictures.letter}`;
+    div.appendChild(label);
+    div.appendChild(input);
+    div.appendChild(img);
+    pictureContainer.appendChild(div);
+  });
+};
+
+// Funkcija, kas pārbauda vai ievadītais burts ir vienāds ar masīvā norādīto burtu.
+function check(event, ui) {
+  var UserInput = document.getElementById( `${pictures.letter}_input` );
+  var ImageInfo = document.getElementById( `${pictures.letter}` );
+  if (UserInput == ImageInfo){
+    correct += 1;
+  }
+  if (correct == 3){
+    const pictureContainer = document.getElementById("picture-container");
+    pictureContainer.innerHTML = '';
+    $('#Ending').show();
+  }
+};
+
+/*
+Vecais kods:
+
+function start(){
+  $('#Ending').hide();
+
+  // Define an array of picture objects, each containing a URL and a letter.
+  const pictures = [
+    {url: "bildes2/abols.jpg", letter: "Ā"},
+    {url: "picture2.jpg", letter: "B"},
+    {url: "picture3.jpg", letter: "C"}
+  ];
+
+  // Pārkārto masīvu
+  for (let i = pictures.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [pictures[i], pictures[j]] = [pictures[j], pictures[i]];
+  }
+
+  // Atsevisks div katrai bildei un input vieta
+  const pictureContainer = document.getElementById("picture-container");
+  pictures.forEach(pictures => {
+    const div = document.createElement("div");
+    const label = document.createElement("label");
+    label.textContent = `Uz kuru burtu šis auglis sākas?`;
+    const input = document.createElement("input");
+    input.type = "teksts";
+    input.maxLength = 1;
+    input.required = true;
+    const img = document.createElement("img");
+    img.src = pictures.url;
+    img.alt = `Bilde ar kaut ko kas sākas uz burtu: ${pictures.letter}`;
+    div.appendChild(label);
+    div.appendChild(input);
+    div.appendChild(img);
+    pictureContainer.appendChild(div);
+  });
+  drop: check
 }
+*/
