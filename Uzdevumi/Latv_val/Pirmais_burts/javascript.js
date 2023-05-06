a847d94f29b0bc18aec7bda2259c079f0bff4003
voamanda <102229544+voamanda@users.noreply.github.com>
2023-05-06 20:28:44 +0300

    Update javascript.js

--- Uzdevumi/Latv_val/Pirmais_burts/javascript.js
+++ Uzdevumi/Latv_val/Pirmais_burts/javascript.js
@@ -8,18 +8,6 @@ var correct = 0;
 
 $( start );
 
-/*
-function check(event, ui) {
-  var UserInput = document.getElementById( `${pictures.letter}_input` );
-  var ImageInfo = document.getElementById( `${pictures.letter}` );
-  if (UserInput == ImageInfo){
-    correct += 1;
-  }
-  if (correct == 3){
-    $('#Ending').show();
-  }
-};*/
-
 function start(){
   correct = 0;
   $('#Ending').hide();
@@ -62,6 +50,9 @@ function check(event, ui) {
     var ImageInfo = document.getElementById( `${pictures.letter}` );
     console.log(UserInput.value);
     console.log(ImageInfo.value);
+    input.addEventListener("focus", function() {
+  input.style = 'border-radius: 20px; border: 4px solid var(--project_green); text-align: center;';
+});
     if (UserInput.value.toUpperCase() == ImageInfo.value){
       correct += 1;
     };
@@ -77,47 +68,3 @@ function check(event, ui) {
     correct = 0;
   };
 };
-input.addEventListener("focus", function() {
-  input.style = 'border-radius: 20px; border: 4px solid var(--project_green); text-align: center;';
-});
-
-/*
-Vecais kods:
-
-function start(){
-  $('#Ending').hide();
-
-  // Define an array of picture objects, each containing a URL and a letter.
-  const pictures = [
-    {url: "bildes2/abols.jpg", letter: "Ā"},
-    {url: "picture2.jpg", letter: "B"},
-    {url: "picture3.jpg", letter: "C"}
-  ];
-
-  // Pārkārto masīvu
-  for (let i = pictures.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [pictures[i], pictures[j]] = [pictures[j], pictures[i]];
-  }
-
-  // Atsevisks div katrai bildei un input vieta
-  const pictureContainer = document.getElementById("picture-container");
-  pictures.forEach(pictures => {
-    const div = document.createElement("div");
-    const label = document.createElement("label");
-    label.textContent = `Uz kuru burtu šis auglis sākas?`;
-    const input = document.createElement("input");
-    input.type = "teksts";
-    input.maxLength = 1;
-    input.required = true;
-    const img = document.createElement("img");
-    img.src = pictures.url;
-    img.alt = `Bilde ar kaut ko kas sākas uz burtu: ${pictures.letter}`;
-    div.appendChild(label);
-    div.appendChild(input);
-    div.appendChild(img);
-    pictureContainer.appendChild(div);
-  });
-  drop: check
-}
-*/
